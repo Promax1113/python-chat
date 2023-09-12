81ee31962662a4894536b04cfb680ad79a20f89c
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2023-09-12 19:15:40 +0200

    test

--- client.py
+++ client.py
@@ -129,6 +129,6 @@ if __name__ == '__main__':
     result = login()
     fernet_obj = result['key']
     username = result['username']
-    print('Awaiting server!!')
+    print('Awaiting server!g')
     await_server(fernet_obj)
     
